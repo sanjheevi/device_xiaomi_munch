7d611c4517964fcbaa199c9507914f3638e06a58
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    munch: support updatable APEX
    Before, updatable_apex.mk was inherited indirectly from
    mainline_system.mk. Since mainline_system.mk no longer inherits
    updatable_apex.mk, updatable_apex.mk is inherited directly from
    device-specific mk file.
    
    Bug: 132664085
    Test: choosecombo to haydn; get_build_var TARGET_FLATTEN_APEX
    shows false
    
    Change-Id: I7162567b732f284f618a77cf09f5b40aadd913d5
    Merged-In: I7162567b732f284f618a77cf09f5b40aadd913d5

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
