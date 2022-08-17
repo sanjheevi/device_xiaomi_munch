3b34df0aa979a7269f47c63b3c8b6dc7e5c74df5
kubersharma001 <kubersharma001@gmail.com>
2022-08-17 04:07:14 +0000

    munch: Inherit developer gsi keys
    error:  build/make/target/product/gsi_keys.mk does not exist..
    
    Change-Id: I7ac37f3e104f1bc6dce504ca29fcbf078127b399

--- device.mk
+++ device.mk
@@ -8,8 +8,8 @@ MUNCH_PREBUILT := device/xiaomi/munch-prebuilt
 
 $(call inherit-product, vendor/xiaomi/munch/munch-vendor.mk)
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
