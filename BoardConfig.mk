dd324e4f4bac79a76c37dc8135ddd02949cef69f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-17 19:04:31 +0530

    munch: Define kernel config
    Co-authored-by: AdarshGrewal <adarshgrewal@gmail.com>
    Co-authored-by: ganeshi4u <ganeshvarmai4u@gmail.com>
    Co-authored-by: madmax7896 <madmax78968@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,32 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := kryo385
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := kona
 
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_BINARIES := kernel
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm 
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc LLVM=1
+TARGET_KERNEL_APPEND_DTB := false
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+
+TARGET_KERNEL_CONFIG := munch_defconfig
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_SOURCE := kernel/xiaomi/munch
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := kona
+
+# Recovery
+TARGET_HAS_GENERIC_KERNEL_HEADERS := true
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_USES_RECOVERY_AS_BOOT := true
