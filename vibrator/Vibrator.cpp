5e6f8925be73ee7715b3dd0fe68e2810d5ca29dd
zoldycc <sanjheevisv@palladiumos.com>
2023-04-15 13:47:39 +0530

    munch: vibrator: adapt vibrator hal for munch
    Signed-off-by: zoldycc <sanjheevisv@palladiumos.com>

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -27,7 +27,7 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define LOG_TAG "vendor.qti.vibrator"
+#define LOG_TAG "vendor.qti.vibrator.munch"
 
 #include <dirent.h>
 #include <inttypes.h>
@@ -467,11 +467,7 @@ ndk::ScopedAStatus Vibrator::perform(Effect effect, EffectStrength es, const std
 
     ALOGD("Vibrator perform effect %d", effect);
 
-#ifdef TARGET_SUPPORTS_OFFLOAD
-    if (effect < Effect::CLICK ||  effect > Effect::RINGTONE_15)
-#else
     if (effect < Effect::CLICK ||  effect > Effect::HEAVY_CLICK)
-#endif
         return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
 
     if (es != EffectStrength::LIGHT && es != EffectStrength::MEDIUM && es != EffectStrength::STRONG)
@@ -498,14 +494,8 @@ ndk::ScopedAStatus Vibrator::getSupportedEffects(std::vector<Effect>* _aidl_retu
     if (ledVib.mDetected)
         return ndk::ScopedAStatus::ok();
 
-#ifdef TARGET_SUPPORTS_OFFLOAD
-    *_aidl_return = {Effect::CLICK, Effect::DOUBLE_CLICK, Effect::TICK, Effect::THUD,
-                     Effect::POP, Effect::HEAVY_CLICK, Effect::RINGTONE_12,
-                     Effect::RINGTONE_13, Effect::RINGTONE_14, Effect::RINGTONE_15};
-#else
     *_aidl_return = {Effect::CLICK, Effect::DOUBLE_CLICK, Effect::TICK, Effect::THUD,
                      Effect::POP, Effect::HEAVY_CLICK};
-#endif
     return ndk::ScopedAStatus::ok();
 }
 
