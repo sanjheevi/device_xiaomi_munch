c7a16d00bc0b590d057a60df0b28afdd0806694c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-10-28 21:36:28 +0530

    munch: libinit: import from SM8250 Common tree
    * Source -> https://github.com/LineageOS/android_device_xiaomi_sm8250-common/tree/lineage-20/libinit
             -> https://github.com/LineageOS/android_device_xiaomi_alioth/tree/lineage-20/libinit
    
    @sanjheevi -> Adapted for munch
    
    Co-Authored-By: Ramii Ahmed <ramy@ahmedramy.com>
    Co-Authored-By: luk1337 <priv.luk@gmail.com>
    Co-Authored-By: Alexander Winkowski <dereference23@outlook.com>
    Co-Authored-By: althafvly <althafvly@gmail.com>
    Signed-off-by: sanjheevi <sanjheevisv@palladiumos.org>

--- libinit/init_munch.cpp
+++ libinit/init_munch.cpp
@@ -1,116 +1,54 @@
 /*
-   Copyright (c) 2015, The Linux Foundation. All rights reserved.
-   Copyright (C) 2016 The CyanogenMod Project.
-   Copyright (C) 2019-2020 The LineageOS Project.
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <fstream>
-#include <unistd.h>
-#include <vector>
+#include <libinit_dalvik_heap.h>
+#include <libinit_variant.h>
 
-#include <android-base/properties.h>
-#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/_system_properties.h>
-
-#include "property_service.h"
 #include "vendor_init.h"
-#include <sys/sysinfo.h>
 
-using android::base::GetProperty;
+static const variant_info_t munchin_info = {
+    .hwc_value = "INDIA",
+    .sku_value = "",
+    .brand = "POCO",
+    .device = "munch",
+    .marketname = "POCO F4",
+    .model = "22021211RI",
+    .build_fingerprint = "POCO/munch_in/munch:13/RKQ1.211001.001/V14.0.6.0.TLMINXM:user/release-keys",
+    .nfc = true,
+};
 
-void property_override(char const prop[], char const value[], bool add = true) {
-    prop_info *pi;
+static const variant_info_t munch_global_info = {
+    .hwc_value = "GLOBAL",
+    .sku_value = "",
+    .brand = "Redmi",
+    .device = "munch",
+    .marketname = "POCO F4",
+    .model = "22021211RG",
+    .build_fingerprint = "Redmi/munch/munch:13/RKQ1.211001.001/V14.0.6.0.TLMINXM:user/release-keys",
+    .nfc = true,
+};
 
-    pi = (prop_info *)__system_property_find(prop);
-    if (pi) {
-        __system_property_update(pi, value, strlen(value));
-    } else if (add) {
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-    }
-}
+static const variant_info_t munch_info = {
+    .hwc_value = "",
+    .sku_value = "",
+    .brand = "Redmi",
+    .device = "munch",
+    .marketname = "Redmi K40S",
+    .model = "22021211RC",
+    .build_fingerprint = "Redmi/munch/munch:13/RKQ1.211001.001/V14.0.6.0.TLMINXM:user/release-keys",
+    .nfc = true,
+};
 
-void load_dalvik_properties() {
-    struct sysinfo sys;
-
-    sysinfo(&sys);
-    if (sys.totalram < 6144ull * 1024 * 1024) {
-        // from - phone-xhdpi-6144-dalvik-heap.mk
-        property_override("dalvik.vm.heapstartsize", "16m");
-        property_override("dalvik.vm.heapgrowthlimit", "256m");
-        property_override("dalvik.vm.heapsize", "512m");
-        property_override("dalvik.vm.heapmaxfree", "32m");
-    } else {
-        // 8GB & 12GB RAM
-        property_override("dalvik.vm.heapstartsize", "32m");
-        property_override("dalvik.vm.heapgrowthlimit", "512m");
-        property_override("dalvik.vm.heapsize", "768m");
-        property_override("dalvik.vm.heapmaxfree", "64m");
-    }
-
-    property_override("dalvik.vm.heaptargetutilization", "0.5");
-    property_override("dalvik.vm.heapminfree", "8m");
-}
-
-void load_munchglobal() {
-    property_override("ro.product.vendor.brand", "Redmi");
-    property_override("ro.product.vendor.marketname", "POCO F4");
-    property_override("ro.product.model", "22021211RG");
-    property_override("ro.build.product", "munch");
-    property_override("ro.product.device", "munch");
-    property_override("ro.build.fingerprint", "Redmi/munch/munch:13/RKQ1.211001.001/V14.0.6.0.TLMINXM:user/release-keys");
-}
-
-void load_munchin() {
-    property_override("ro.product.vendor.brand", "POCO");
-    property_override("ro.product.vendor.marketname", "POCO F4");
-    property_override("ro.product.model", "22021211RI");
-    property_override("ro.build.product", "munch");
-    property_override("ro.product.device", "munch");
-    property_override("ro.build.fingerprint", "POCO/munch_in/munch:13/RKQ1.211001.001/V14.0.6.0.TLMINXM:user/release-keys");
-}
-
-void load_munch() {
-    property_override("ro.product.vendor.brand", "Redmi");
-    property_override("ro.product.vendor.marketname", "Redmi K40S");
-    property_override("ro.product.model", "22021211RC");
-    property_override("ro.build.product", "munch");
-    property_override("ro.product.device", "munch");
-    property_override("ro.build.fingerprint", "Redmi/munch/munch:13/RKQ1.211001.001/V14.0.6.0.TLMINXM:user/release-keys");
-}
-
+static const std::vector<variant_info_t> variants = {
+    munchin_info,
+    munch_global_info,
+    munch_info,
+};
 
 void vendor_load_properties() {
-    std::string region = android::base::GetProperty("ro.boot.hwc", "");
-
-    if (region.find("INDIA") != std::string::npos) {
-        load_munchin();
-    } else if (region.find("GLOBAL") != std::string::npos) {
-        load_munchglobal();
-    } else {
-      load_munch();
-    }
-    load_dalvik_properties();
+    search_variant(variants);
+    set_dalvik_heap();
 }
